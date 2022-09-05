2c038ab680a45963925535b48897668f3e4e15c3
Kuzmich87 <d.kuzmich87@gmail.com>
2022-09-05 19:18:24 +0300

    Finish 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,42 @@
-import throtle from 'lodash.throttle';
+
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 
-formRef.addEventListener('input', throtle(onFormInput, 500));
-formRef.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const FORM__KEY = 'feedback-form-state';
 
 const formData = {};
+dataFromLocalStorage()
+
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FORM__KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM__KEY);
+  console.log(JSON.parse(localStorage.getItem(FORM__KEY)));
+}
+
+
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(FORM__KEY));
+    if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+};
+
+
+
+
+
+
