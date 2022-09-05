d72e0e750801f77d1a2268471aac423263ba3f03
Kuzmich87 <d.kuzmich87@gmail.com>
2022-09-05 15:18:11 +0300

    Задача 2 готова

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,21 +8,13 @@ const PLAYER__KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay( seconds ) {
-  localStorage.setItem(PLAYER__KEY, seconds);
+function onPlay({ seconds }) {
+  localStorage.setItem(PLAYER__KEY,  seconds );
+  
 }
 
+const savedTime =  Number(localStorage.getItem(PLAYER__KEY));
 
-player.setCurrentTime(localStorage.getItem(PLAYER__KEY)).then(function(PLAYER__KEY) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+ if (savedTime) {
+    player.setCurrentTime(savedTime); 
+ }
