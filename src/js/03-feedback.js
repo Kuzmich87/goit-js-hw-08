1ddabb30f25449bd40770565850347ef2453f397
Kuzmich87 <d.kuzmich87@gmail.com>
2022-09-06 09:22:01 +0300

    Fics

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,56 @@
 
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
-
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
-
 const FORM__KEY = 'feedback-form-state';
+let formData = {};
+const refs = {
+  inputFormFeedback: document.querySelector('.feedback-form'),
+  inputFormEmail: document.querySelector('[type="email"]'),
+  inputFormMessage: document.querySelector('[name="message"]'),
+  inputFormSubmitBtn: document.querySelector('[type="submit"]'),
+};
+populateTextarea();
+refs.inputFormFeedback.addEventListener('submit', onFormSubmit);
+refs.inputFormFeedback.addEventListener('input', throttle(onInputStorage, 500));
 
-const formData = {};
-dataFromLocalStorage()
-
-
-function onFormData(e) {
+// * convert to string and safe to FORM__KEY
+function onInputStorage(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(FORM__KEY, JSON.stringify(formData));
 }
 
-function onSubmitForm(e) {
+// * work with button submit
+function onFormSubmit(evt) {
   
-  e.preventDefault();
-  e.currentTarget.reset();
+  // * 1 add prevent default
+console.log(formData);
+evt.preventDefault();
+
+// *  check if some input filled if not warning
+  const formElements = evt.currentTarget.elements;
+  const emailValue = formElements.email.value;
+  const messageValue = formElements.message.value;
+  if (emailValue === '' || messageValue === '') {
+    alert('Все поля ввода должны быть заполнены. Исправьте, пожалуйста.');
+  };
+
+// *  clear all fields
+  evt.currentTarget.reset();
   localStorage.removeItem(FORM__KEY);
-  console.log(JSON.parse(localStorage.getItem(FORM__KEY)));
-}
-
-
-function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem(FORM__KEY));
-    if (data) {
-    email.value = data.email || '';
-    message.value = data.message || '';
-  }
+  formData = {};
 };
 
+// *  get from local storage and receive variables to names
+function populateTextarea() {
+
+  const savedStorageInputs = localStorage.getItem(FORM__KEY)
+ 
+  if (savedStorageInputs) {
+    formData = JSON.parse(savedStorageInputs);
+    refs.inputFormEmail.value = formData.email || '';
+    refs.inputFormMessage.value = formData.message|| '';
+
+}}
 
 
 
